40e42f1feae12080340c04f04568c5752394f9bb
Ntkskwk <natsukawa247@outlook.com>
2024-03-18 23:21:45 +0800

    feat: get location of user

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <p>{{ location }}</p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import axios from 'axios'
 export default {
+  mounted() {
+    this.getLocation();
+  },
+
+
+  data() {
+    return {
+      location: ''
+    }
+  },
+
   name: 'Dashboard',
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+
+  methods: {
+    getLocation() {
+      axios.get('https://ipapi.co/json')
+        .then(response => {
+          console.log(response)
+          this.location = response.data.city
+        })
+        .catch(error => (console.log(error)))
+    },
   }
 }
 </script>
